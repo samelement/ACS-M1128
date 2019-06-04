e83847a0e6759eb71efc3d761302ca6faafe2776
SAM Element <47309504+samelement@users.noreply.github.com>
2019-06-04 19:05:03 +0700

    Add files via upload

--- examples/GasLeakageAlarm/GasLeakageAlarm.ino
+++ examples/GasLeakageAlarm/GasLeakageAlarm.ino
@@ -22,7 +22,7 @@
 
 WiFiClientSecure wclientSecure;
 PubSubClient client(wclientSecure,MQTT_BROKER_HOST,MQTT_BROKER_PORT_TLS);
-HardwareSerial SerialDEBUG = Serial;
+HardwareSerial *SerialDEBUG = &Serial;
 M1128 obj;
 
 bool pinLastState = DEVICE_PIN_DEFSTATE;
@@ -30,9 +30,9 @@ bool pinCurrentState = DEVICE_PIN_DEFSTATE;
 
 void setup() {
   if (DEBUG) {
-    SerialDEBUG.begin(DEBUG_BAUD, SERIAL_8N1, SERIAL_TX_ONLY);
+    SerialDEBUG->begin(DEBUG_BAUD, SERIAL_8N1, SERIAL_TX_ONLY);
     while (!SerialDEBUG);
-    SerialDEBUG.println("Initializing..");
+    SerialDEBUG->println("Initializing..");
   }
   client.set_callback(callbackOnReceive);
   pinMode(DEVICE_PIN_INPUT,INPUT);
@@ -48,7 +48,7 @@ void setup() {
   obj.onAPTimeout = callbackOnAPTimeout;
   obj.onWiFiTimeout = callbackOnWiFiTimeout;  
   ESP.wdtEnable(8000);      
-  obj.init(client,true,SerialDEBUG); //pass client, set clean_session=true, use debug.
+  obj.init(client,true,true,SerialDEBUG); //pass client, set clean_session=true, set lwt=true, use debug.
   delay(10);
 }
 
@@ -60,10 +60,10 @@ void loop() {
 
 void callbackOnReceive(const MQTT::Publish& pub) {
   if (DEBUG) {
-    SerialDEBUG.print(F("Receiving topic: "));
-    SerialDEBUG.println(pub.topic());
-    SerialDEBUG.print("With value: ");
-    SerialDEBUG.println(pub.payload_string());
+    SerialDEBUG->print(F("Receiving topic: "));
+    SerialDEBUG->println(pub.topic());
+    SerialDEBUG->print("With value: ");
+    SerialDEBUG->println(pub.payload_string());
   }
   if (pub.topic()==obj.constructTopic("reset") && pub.payload_string()=="true") obj.reset();
   else if (pub.topic()==obj.constructTopic("restart") && pub.payload_string()=="true") obj.restart();
