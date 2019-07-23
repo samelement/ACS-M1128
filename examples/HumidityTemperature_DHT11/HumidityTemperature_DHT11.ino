d273837ca649a59ab06a4966bc20364cc9c7ed49
SAM Element <47309504+samelement@users.noreply.github.com>
2019-07-23 11:45:01 +0700

    Add files via upload

---
+++ examples/HumidityTemperature_DHT11/HumidityTemperature_DHT11.ino
@@ -0,0 +1,206 @@
+/* 
+ *  DHT11 sensor example with ESP8266-01:
+ *  
+ *  1. Publish message every sensorDelayMS to "/sensor/temp" and "/sensor/humid" for temperature and humidity.
+ *  2. Connect DHT11 data out to GPIO0.
+ *  3. This example use Adafruit unified sensor library for DHT11 reading, hence you must install it first.
+ *  
+ */
+
+#include "M1128.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DEBUG true
+#define DEBUG_BAUD 9600
+
+#define DEVELOPER_ID "1"
+#define DEVELOPER_USER "dmI0OkvoFRLRzHu3J3tEWQbIXQwDeF9q"
+#define DEVELOPER_PASS "dyUiAb1cjkS8FRrokTXxtY1s4DUmOJsa"
+
+#define WIFI_DEFAULT_SSID "SmartDHT11"
+#define WIFI_DEFAULT_PASS "abcd1234"
+
+WiFiClientSecure wclientSecure;
+PubSubClient client(wclientSecure, MQTT_BROKER_HOST, MQTT_BROKER_PORT_TLS);
+HardwareSerial *SerialDEBUG = &Serial;
+M1128 obj;
+
+#define DHTPIN            0         // Pin which is connected to the DHT sensor.
+#define DHTTYPE           DHT11     // DHT 11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+unsigned int sensorDelayMS = 0;
+unsigned int sensorCurMillis = 0;
+unsigned int sensorPrevMillis = 0;
+
+void setup() {
+  if (DEBUG) {
+    SerialDEBUG->begin(DEBUG_BAUD, SERIAL_8N1, SERIAL_TX_ONLY);
+    while (!SerialDEBUG);
+    SerialDEBUG->println("Initializing..");
+  }
+  client.set_callback(callbackOnReceive);
+  pinMode(DHTPIN,INPUT);
+  pinMode(3, FUNCTION_3);
+  obj.pinReset = 3;
+  obj.apConfigTimeout = 300000;
+  obj.wifiConnectTimeout = 120000;
+  obj.wifiClientSecure = &wclientSecure;
+  obj.devConfig(DEVELOPER_ID,DEVELOPER_USER,DEVELOPER_PASS);
+  obj.wifiConfig(WIFI_DEFAULT_SSID,WIFI_DEFAULT_PASS);
+  obj.onConnect = callbackOnConnect;  
+  obj.onReconnect = callbackOnReconnect;
+  obj.onAPConfigTimeout = callbackOnAPConfigTimeout;
+  obj.onWiFiConnectTimeout = callbackOnWiFiConnectTimeout;
+  ESP.wdtEnable(8000);
+  initSensors();
+  obj.init(client,true,true,SerialDEBUG); //pass client, set clean_session=true, set lwt=true, use debug.
+  delay(10);
+}
+
+void loop() {
+  ESP.wdtFeed();
+  obj.loop();
+  if (obj.isReady) measureSensors();
+}
+
+void initSensors() {
+  SerialDEBUG->println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  SerialDEBUG->println("------------------------------------");
+  SerialDEBUG->println("Temperature");
+  SerialDEBUG->print  ("Sensor:       "); SerialDEBUG->println(sensor.name);
+  SerialDEBUG->print  ("Driver Ver:   "); SerialDEBUG->println(sensor.version);
+  SerialDEBUG->print  ("Unique ID:    "); SerialDEBUG->println(sensor.sensor_id);
+  SerialDEBUG->print  ("Max Value:    "); SerialDEBUG->print(sensor.max_value); SerialDEBUG->println(" *C");
+  SerialDEBUG->print  ("Min Value:    "); SerialDEBUG->print(sensor.min_value); SerialDEBUG->println(" *C");
+  SerialDEBUG->print  ("Resolution:   "); SerialDEBUG->print(sensor.resolution); SerialDEBUG->println(" *C");  
+  SerialDEBUG->println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  SerialDEBUG->println("------------------------------------");
+  SerialDEBUG->println("Humidity");
+  SerialDEBUG->print  ("Sensor:       "); SerialDEBUG->println(sensor.name);
+  SerialDEBUG->print  ("Driver Ver:   "); SerialDEBUG->println(sensor.version);
+  SerialDEBUG->print  ("Unique ID:    "); SerialDEBUG->println(sensor.sensor_id);
+  SerialDEBUG->print  ("Max Value:    "); SerialDEBUG->print(sensor.max_value); SerialDEBUG->println("%");
+  SerialDEBUG->print  ("Min Value:    "); SerialDEBUG->print(sensor.min_value); SerialDEBUG->println("%");
+  SerialDEBUG->print  ("Resolution:   "); SerialDEBUG->print(sensor.resolution); SerialDEBUG->println("%");  
+  SerialDEBUG->println("------------------------------------");
+  // Set delay between sensor readings based on sensor details.
+  //sensorDelayMS = sensor.min_delay / 1000;
+  sensorDelayMS = 60000;
+}
+
+void measureSensors() {
+  // Delay between measurements.
+  sensorCurMillis = millis();
+  if ((sensorCurMillis - sensorPrevMillis > sensorDelayMS) || (sensorCurMillis - sensorPrevMillis < 0)) {
+    sensorPrevMillis = sensorCurMillis;
+    char result[6]; // Buffer big enough for 6-character float
+    // Get temperature event and print its value.
+    sensors_event_t event;  
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      SerialDEBUG->println("Error reading temperature!");
+    }
+    else {
+      SerialDEBUG->print("Temperature: ");
+      SerialDEBUG->print(event.temperature);
+      SerialDEBUG->println(" °C");
+      dtostrf(event.temperature, 4, 2, result);
+      client.publish(MQTT::Publish(obj.constructTopic("sensor/temp"), result).set_retain(true).set_qos(1));
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      SerialDEBUG->println("Error reading humidity!");
+    }
+    else {
+      SerialDEBUG->print("Humidity: ");
+      SerialDEBUG->print(event.relative_humidity);
+      SerialDEBUG->println("%");
+      dtostrf(event.relative_humidity, 4, 2, result);
+      client.publish(MQTT::Publish(obj.constructTopic("sensor/humid"), result).set_retain(true).set_qos(1));
+    }
+  }
+}
+
+void callbackOnReceive(const MQTT::Publish& pub) {
+  if (DEBUG) {
+    SerialDEBUG->print(F("Receiving topic: "));
+    SerialDEBUG->println(pub.topic());
+    SerialDEBUG->print("With value: ");
+    SerialDEBUG->println(pub.payload_string());
+  }
+  if (pub.topic()==obj.constructTopic("reset") && pub.payload_string()=="true") obj.reset();
+  else if (pub.topic()==obj.constructTopic("restart") && pub.payload_string()=="true") obj.restart();
+}
+
+void callbackOnConnect() {
+  initPublish();    
+  initSubscribe();
+}
+
+void callbackOnReconnect() {
+  initSubscribe();
+}
+
+void callbackOnAPConfigTimeout() {
+  obj.restart();
+}
+
+void callbackOnWiFiConnectTimeout() {
+  ESP.deepSleep(300000000); // sleep for 5 minutes
+}
+
+void publishState(const char* state) {
+  if (client.connected()) client.publish(MQTT::Publish(obj.constructTopic("$state"), state).set_retain().set_qos(1));  
+}
+
+void initPublish() {
+  if (client.connected()) {    
+    client.publish(MQTT::Publish(obj.constructTopic("$state"), "init").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$sammy"), "1.0.0").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$name"), "Smart DHT11").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$model"), "SAM-DHT11").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$mac"), WiFi.macAddress()).set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$localip"), WiFi.localIP().toString()).set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$fw/name"), "DHT11").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$fw/version"), "1.00").set_retain(false).set_qos(1));    
+    client.publish(MQTT::Publish(obj.constructTopic("$reset"), "true").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$restart"), "true").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("$nodes"), "sensor").set_retain(false).set_qos(1));
+  
+  //define node "sensor"
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/$name"), "Sensor").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/$type"), "Sensor-01").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/$properties"), "temp,humid").set_retain(false).set_qos(1));
+
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$name"), "Temperature").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$settable"), "false").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$retained"), "true").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$datatype"), "float").set_retain(false).set_qos(1));  
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$unit"), "°C").set_retain(false).set_qos(1));  
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/temp/$format"), "-40:125").set_retain(false).set_qos(1));  
+
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$name"), "Humidity").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$settable"), "false").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$retained"), "true").set_retain(false).set_qos(1));
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$datatype"), "float").set_retain(false).set_qos(1));    
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$unit"), "%").set_retain(false).set_qos(1));  
+    client.publish(MQTT::Publish(obj.constructTopic("sensor/humid/$format"), "0:100").set_retain(false).set_qos(1));  
+  }
+}
+
+void initSubscribe() {
+  if (client.connected()) {
+    //default, must exist
+    client.subscribe(MQTT::Subscribe().add_topic(obj.constructTopic("reset"),2));  
+    client.subscribe(MQTT::Subscribe().add_topic(obj.constructTopic("restart"),2));  
+  }
+  publishState("ready");
+}
