51570d6a0347c0bd775382f038abffcc5687f955
SAM Element <47309504+samelement@users.noreply.github.com>
2019-08-26 13:42:51 +0700

    Add files via upload

--- examples/GasLeakageAlarm/GasLeakageAlarm.ino
+++ examples/GasLeakageAlarm/GasLeakageAlarm.ino
@@ -21,10 +21,8 @@
 #define DEVICE_PIN_INPUT 0 //GPIO pin input
 #define DEVICE_PIN_OUTPUT 2 // pin GPIO2 for output
 
-WiFiClientSecure wclientSecure;
-PubSubClient client(wclientSecure,MQTT_BROKER_HOST,MQTT_BROKER_PORT_TLS);
 HardwareSerial *SerialDEBUG = &Serial;
-M1128 obj;
+M1128 iot;
 
 bool pinLastState = DEVICE_PIN_DEFSTATE;
 bool pinCurrentState = DEVICE_PIN_DEFSTATE;
@@ -35,41 +33,49 @@ void setup() {
     while (!SerialDEBUG);
     SerialDEBUG->println("Initializing..");
   }
-  client.set_callback(callbackOnReceive);
   pinMode(DEVICE_PIN_INPUT,INPUT);
   pinMode(DEVICE_PIN_OUTPUT,OUTPUT);
   digitalWrite(DEVICE_PIN_OUTPUT, DEVICE_PIN_DEFSTATE);
   pinMode(3, FUNCTION_3);
-  obj.pinReset = 3;
-  obj.apConfigTimeout = 300000;
-  obj.wifiConnectTimeout = 120000;
-  obj.wifiClientSecure = &wclientSecure;    
-  obj.devConfig(DEVELOPER_ROOT,DEVELOPER_USER,DEVELOPER_PASS);
-  obj.wifiConfig(WIFI_DEFAULT_SSID,WIFI_DEFAULT_PASS);
-  obj.onConnect = callbackOnConnect;
-  obj.onReconnect = callbackOnReconnect;
-  obj.onAPConfigTimeout = callbackOnAPConfigTimeout;
-  obj.onWiFiConnectTimeout = callbackOnWiFiConnectTimeout;  
+  iot.pinReset = 3;
+  iot.prod = false;
+  iot.cleanSession = true;
+  iot.setWill = true;
+  iot.apConfigTimeout = 300000;
+  iot.wifiConnectTimeout = 120000;
+  iot.devConfig(DEVELOPER_ROOT,DEVELOPER_USER,DEVELOPER_PASS);
+  iot.wifiConfig(WIFI_DEFAULT_SSID,WIFI_DEFAULT_PASS);
+  
+  iot.onReceive = callbackOnReceive;
+  iot.onConnect = callbackOnConnect;
+  iot.onReconnect = callbackOnReconnect;
+  iot.onAPConfigTimeout = callbackOnAPConfigTimeout;
+  iot.onWiFiConnectTimeout = callbackOnWiFiConnectTimeout;  
+  
   ESP.wdtEnable(8000);      
-  obj.init(client,true,true,SerialDEBUG); //pass client, set clean_session=true, set lwt=true, use debug.
+  iot.init(DEBUG?SerialDEBUG:NULL);
   delay(10);
 }
 
 void loop() {
   ESP.wdtFeed();
-  obj.loop();
+  iot.loop();
   checkSensor();
 }
 
-void callbackOnReceive(const MQTT::Publish& pub) {
+void callbackOnReceive(char* topic, byte* payload, unsigned int length) {
+  String strPayload;
+  strPayload.reserve(length);
+  for (uint32_t i = 0; i < length; i++) strPayload += (char)payload[i];
+
   if (DEBUG) {
     SerialDEBUG->print(F("Receiving topic: "));
-    SerialDEBUG->println(pub.topic());
+    SerialDEBUG->println(topic);
     SerialDEBUG->print("With value: ");
-    SerialDEBUG->println(pub.payload_string());
+    SerialDEBUG->println(strPayload);
   }
-  if (pub.topic()==obj.constructTopic("reset") && pub.payload_string()=="true") obj.reset();
-  else if (pub.topic()==obj.constructTopic("restart") && pub.payload_string()=="true") obj.restart();
+  if (topic==iot.constructTopic("reset") && strPayload=="true") iot.reset();
+  else if (topic==iot.constructTopic("restart") && strPayload=="true") iot.restart();
 }
 
 void callbackOnConnect() {
@@ -82,64 +88,62 @@ void callbackOnReconnect() {
 }
 
 void callbackOnAPConfigTimeout() {
-  obj.restart();
+  iot.restart();
 }
 
 void callbackOnWiFiConnectTimeout() {
-  obj.restart();
+  iot.restart();
 }
 
 void checkSensor() {
-  if (!client.connected()) return;
   pinCurrentState = digitalRead(DEVICE_PIN_INPUT);
   if (pinLastState==!DEVICE_PIN_DEFSTATE && pinCurrentState!=pinLastState) { // alarm ON   
     digitalWrite(DEVICE_PIN_OUTPUT, !DEVICE_PIN_DEFSTATE); // make noisy board alarm
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg"), "true").set_retain().set_qos(1)); 
+    if (iot.mqtt->connected()) iot.mqtt->publish(iot.constructTopic("sensor/lpg"), "true", true); 
   } else if (pinLastState==DEVICE_PIN_DEFSTATE && pinCurrentState!=pinLastState) {  // alarm OFF  
     digitalWrite(DEVICE_PIN_OUTPUT, DEVICE_PIN_DEFSTATE); // turn off board alarm
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg"), "false").set_retain().set_qos(1)); 
+    if (iot.mqtt->connected()) iot.mqtt->publish(iot.constructTopic("sensor/lpg"), "false", true); 
   }
   pinLastState = pinCurrentState;  
 }
 
 void publishState(const char* state) {
-  if (client.connected()) client.publish(MQTT::Publish(obj.constructTopic("$state"), state).set_retain().set_qos(1));  
+  if (iot.mqtt->connected()) iot.mqtt->publish(iot.constructTopic("$state"), state, true);  
 }
 
 void initPublish() {
-  if (client.connected()) {    
-    client.publish(MQTT::Publish(obj.constructTopic("$state"), "init").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$sammy"), "1.0.0").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$name"), "Gas Alarm").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$model"), "SAM-GLA01").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$mac"), WiFi.macAddress()).set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$localip"), WiFi.localIP().toString()).set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$fw/name"), "GLA01").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$fw/version"), "1.00").set_retain(false).set_qos(1));    
-    client.publish(MQTT::Publish(obj.constructTopic("$reset"), "true").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$restart"), "true").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("$nodes"), "sensor").set_retain(false).set_qos(1));
+  if (iot.mqtt->connected()) {     
+    iot.mqtt->publish(iot.constructTopic("$state"), "init", false);
+    iot.mqtt->publish(iot.constructTopic("$sammy"), "1.0.0", false);
+    iot.mqtt->publish(iot.constructTopic("$name"), "Gas Alarm", false);
+    iot.mqtt->publish(iot.constructTopic("$model"), "SAM-GLA01", false);
+    iot.mqtt->publish(iot.constructTopic("$mac"), WiFi.macAddress().c_str(), false);
+    iot.mqtt->publish(iot.constructTopic("$localip"), WiFi.localIP().toString().c_str(), false);
+    iot.mqtt->publish(iot.constructTopic("$fw/name"), "GLA01", false);
+    iot.mqtt->publish(iot.constructTopic("$fw/version"), "1.00", false);    
+    iot.mqtt->publish(iot.constructTopic("$reset"), "true", false);
+    iot.mqtt->publish(iot.constructTopic("$restart"), "true", false);
+    iot.mqtt->publish(iot.constructTopic("$nodes"), "sensor", false);
   
   //define node "sensor"
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/$name"), "Sensor").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/$type"), "Sensor-01").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/$properties"), "lpg").set_retain(false).set_qos(1));
+    iot.mqtt->publish(iot.constructTopic("sensor/$name"), "Sensor", false);
+    iot.mqtt->publish(iot.constructTopic("sensor/$type"), "Sensor-01", false);
+    iot.mqtt->publish(iot.constructTopic("sensor/$properties"), "lpg", false);
 
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg/$name"), "LPG").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg/$settable"), "false").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg/$retained"), "true").set_retain(false).set_qos(1));
-    client.publish(MQTT::Publish(obj.constructTopic("sensor/lpg/$datatype"), "boolean").set_retain(false).set_qos(1));  
+    iot.mqtt->publish(iot.constructTopic("sensor/lpg/$name"), "LPG", false);
+    iot.mqtt->publish(iot.constructTopic("sensor/lpg/$settable"), "false", false);
+    iot.mqtt->publish(iot.constructTopic("sensor/lpg/$retained"), "true", false);
+    iot.mqtt->publish(iot.constructTopic("sensor/lpg/$datatype"), "boolean", false);  
 
   // set device to ready
-    client.publish(MQTT::Publish(obj.constructTopic("$state"), "ready").set_retain(false).set_qos(1));  
+    iot.mqtt->publish(iot.constructTopic("$state"), "ready", false);  
   }
 }
 
 void initSubscribe() {
-  if (client.connected()) {
-    //default, must exist
-    client.subscribe(MQTT::Subscribe().add_topic(obj.constructTopic("reset"),2));  
-    client.subscribe(MQTT::Subscribe().add_topic(obj.constructTopic("restart"),2));  
+  if (iot.mqtt->connected()) { 
+    iot.mqtt->subscribe(iot.constructTopic("reset"),1);  
+    iot.mqtt->subscribe(iot.constructTopic("restart"),1);  
   }
   publishState("ready");
 }
